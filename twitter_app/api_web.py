3dd6ffbdd00cd28a58104ac09c18d77ac71d5132
Arblade <59773618+Arblade@users.noreply.github.com>
2021-02-04 20:12:41 +0100

    Add files via upload

--- twitter_app/api_web.py
+++ twitter_app/api_web.py
@@ -20,21 +20,21 @@ def search(data,letype,liste):
     # init api twitter
     key1, key2, key3, key4 = get_keys()
     app=Api(key1, key2, key3, key4)
-    num_res=25
+    num_res=24
     rt_enabled=False
     #recehrche twitter
     tweets=app.search_string(string,letype,liste,num_res,rt_enabled)
     #resultats
-    res,details,stats_time,stats_words,list_media=get_pd(tweets)
+    res,details,stats_time,stats_words,list_media,list_links=get_pd(tweets)
     #print("[+] String here > ",res['tweetText'].to_list())
-    stringpd= res['tweetText'].to_list()
+    #stringpd= res['tweetText'].to_list()
     res=pd.DataFrame.to_json(res)[:-1]+",\"stats_time\":\""+str(stats_time)+"\","+json.dumps({"stats_words" : stats_words})[1:-1]+","+json.dumps({"medias":list_media})[1:]
     #df=pd.DataFrame()
     #res['stats_time']=stats_time
     
     # ancienne version dessous
     #res="<br>".join(res.tweetText.tolist())
-    return res,stringpd
+    return res,list_links
 
 def get_keys():
     resultat=[]
